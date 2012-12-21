672a66f30aa68d9a1fe34bfc1caca61a6f5373f9
Matthew Beale <matt.beale@madhatted.com>
2012-12-21 15:22:14 -0500

    Initial checkin

---
+++ README.md
@@ -0,0 +1,42 @@
+**after_image_load** is a small JavaScript library providing a reliable
+callback after an image is loaded. It is dependent on jQuery. This library
+handles several quirks:
+
+* Handle cached data. In modern browsers, cached data may not trigger a
+`load` event. We check for that state, and force the load event to fire.
+* Handle CORS. Any image URL not on the `window.location`'s host will
+have the `crossOrigin` attribute set.
+* Return immediately for data urls.
+
+Usage is simple:
+
+``` javascript
+// This creates an img on the fly
+$.afterImageLoad('http://google.com/foo.png', function(img){
+  // `img` is a jQuery image object.
+});
+
+// It also accepts an image argument
+var rawImg = new Image();
+$.afterImageLoad(rawImg, 'http://google.com/foo.png', function(img){
+  // `img` is a jQuery image object.
+});
+
+// And you can use via the normal jQuery syntax
+var rawImg = new Image();
+$(rawImg).afterImageLoad('http://google.com/foo.png', function(img){
+  // `img` is the image wrapped as jQuery.
+});
+```
+
+Contributing
+------------
+
+Fork, clone, write some code, commit, push, send a pull request. This is
+a quick, young script, and items like CORs may be unreliable across all
+browsers. This is due to bugs that don't have known work-arounds yet. So
+lend a hand.
+
+This script is partially based on techniques in: https://github.com/desandro/imagesloaded
+
+This script is licensed under the MIT license.
---
+++ after_image_load.js
@@ -0,0 +1,74 @@
+;(function($, undefined){
+  'use strict';
+
+  var blankImage = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+
+  // Provides this syntax:
+  //
+  //   $(img).afterImageLoad(someUrl, callback);
+  //
+  $.fn.afterImageLoad = function(newSrc, callback) {
+
+    var img = this,
+        scannableSrc = newSrc.slice(0, 512);
+
+    // Data URLs can be considered loaded.
+    //
+    if (scannableSrc.indexOf('base64') > 0) {
+      img.attr('src', newSrc);
+      callback(img);
+
+      // Returns img
+      //
+      return img;
+    }
+
+    // Other domains need crossOrigin.
+    //
+    if (scannableSrc.indexOf(window.location.host) < 0) {
+      img.attr('crossOrigin', '');
+    }
+
+    // For other requests, attach a handler then
+    // fire the src. If it is cached, quickly swap
+    // it with a blank data-uri to trigger the
+    // onload.
+    //
+    var loadedHandler = function(){
+      if (img.attr('src') != blankImage) {
+        img.unbind('load', loadedHandler);
+        callback(img);
+      }
+    }
+
+    img.bind('load', loadedHandler);
+
+    img.attr('src', newSrc);
+
+    if (img[0].complete || img[0].readyState) {
+      img.attr('src', blankImage);
+      img.attr('src', newSrc);
+    }
+
+    // Returns img
+    //
+    return img;
+  }
+
+  // Provides two additional syntaxes:
+  //
+  //   $.afterImageLoad(imgEl, someUrl, callback);
+  //   $.afterImageLoad(someUrl, callback);
+  //
+  $.afterImageLoad = function(img, newSrc, callback) {
+    if (!callback && $.type(img) == 'string') {
+      var callback = newSrc,
+          newSrc = img,
+          img = $('<img />');
+    } else {
+      var img = $(img);
+    }
+    img.afterImageLoad(newSrc, callback);
+  }
+
+}(jQuery));
